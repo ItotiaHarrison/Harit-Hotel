f2a5b86751a8d927e5e7278fa189d87e6d44ebfc
Harrison Itotia <harrisonitotia@gmail.com>
2024-07-27 21:43:18 +0300

    create the hero section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection/HeroSection";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
-}
+    <>
+      <HeroSection />
+    </>
+  )
+};
+
+export default Home;
