b1f1372b81e84073f7e829498f575d1b02ccdc50
Harrison Itotia <harrisonitotia@gmail.com>
2024-08-03 01:39:36 +0300

    install swr, implement search functionality and display filtered room page

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,12 +1,26 @@
-import { Room } from "@/app/models/room";
+
+import { Room } from "@/models/room";
 import sanityClient from "./sanity";
 import * as queries from './sanityQueries';
 
 export async function getFeaturedRoom() {
     const result = await sanityClient.fetch<Room>(
-        queries.getFeaturedRoomQuery, 
+        queries.getFeaturedRoomQuery,
         {},
-        {cache: 'no-cache'}
+        { cache: 'no-cache' }
     );
     return result;
 }
+
+export async function getRooms() {
+    const result = await sanityClient.fetch<Room[]>(queries.getRoomsQuery, {},
+        { cache: 'no-cache' });
+    return result;
+}
+
+export async function getRoom(slug: string) {
+    const result = await sanityClient.fetch<Room>(queries.getRoom, { slug },
+        {cache: 'no-cache'});
+    return result;
+}
+
